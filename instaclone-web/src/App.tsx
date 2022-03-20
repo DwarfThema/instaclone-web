1798833556e77cdc9d972d4b41fe4103847796e0
Junho Park <dwarfthema@gmail.com>
2022-03-20 22:59:54 +0900

    ~8.4 Router_2

--- instaclone-web/src/App.tsx
+++ instaclone-web/src/App.tsx
@@ -1,13 +1,19 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
 
 function App() {
+  const isLoggedIn = true;
+
   return (
     <div>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
