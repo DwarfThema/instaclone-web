fe7f18710a2ba1e8726b506b1c7660fa04d94be8
Junho Park <dwarfthema@gmail.com>
2022-03-20 22:22:37 +0900

    ~8.3 Router Setup_1

--- instaclone-web/src/App.tsx
+++ instaclone-web/src/App.tsx
@@ -1,5 +1,15 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+        </Routes>
+      </HashRouter>
+    </div>
+  );
 }
 
 export default App;
