600dffa0b3a51ba3182e0f35d75b804cd2cf4a21
Junho Park <dwarfthema@gmail.com>
2022-03-21 18:56:26 +0900

    9.0 styled Component typeScript

--- instaclone-web/src/styles.ts
+++ instaclone-web/src/styles.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, DefaultTheme } from "styled-components";
 import reset from "styled-reset";
 
 export const GlobalStyles = createGlobalStyle`
@@ -8,12 +8,12 @@ export const GlobalStyles = createGlobalStyle`
       }
   `;
 
-export const lightTheme = {
+export const lightTheme: DefaultTheme = {
   fontColor: "#2c2c2c",
   bgColor: "lightgray",
 };
 
-export const darkTheme = {
+export const darkTheme: DefaultTheme = {
   fontColor: "lightgray",
   bgColor: "#2c2c2c",
 };
