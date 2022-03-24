68685875126ed1433a12a8f80a374629ae1d140b
Junho Park <dwarfthema@gmail.com>
2022-03-24 17:07:11 +0900

    ~10.9 Apollo - React Connect

--- instaclone-web/src/App.tsx
+++ instaclone-web/src/App.tsx
@@ -1,8 +1,8 @@
-import { useReactiveVar } from "@apollo/client";
+import { ApolloProvider, useReactiveVar } from "@apollo/client";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { darkModeVar, isLoggedInVar } from "./apollo";
+import { client, darkModeVar, isLoggedInVar } from "./apollo";
 import routes from "./routes";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
@@ -14,23 +14,25 @@ function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
   return (
-    <HelmetProvider>
-      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-        <GlobalStyles />
-        <BrowserRouter>
-          <Routes>
-            {!isLoggedIn ? (
-              <Route path={routes.signUp} element={<SignUp />} />
-            ) : null}
-            <Route
-              path={routes.home}
-              element={isLoggedIn ? <Home /> : <Login />}
-            />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-    </HelmetProvider>
+    <ApolloProvider client={client}>
+      <HelmetProvider>
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+          <GlobalStyles />
+          <BrowserRouter>
+            <Routes>
+              {!isLoggedIn ? (
+                <Route path={routes.signUp} element={<SignUp />} />
+              ) : null}
+              <Route
+                path={routes.home}
+                element={isLoggedIn ? <Home /> : <Login />}
+              />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+      </HelmetProvider>
+    </ApolloProvider>
   );
 }
 
