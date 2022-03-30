785d88358b4e48e31f52a130d5ad0617e827fc42
Junho Park <dwarfthema@gmail.com>
2022-03-30 12:35:46 +0900

    ~11.13 split map text()

--- instaclone-web/src/components/feed/Comment.tsx
+++ instaclone-web/src/components/feed/Comment.tsx
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 import { FatText } from "../shared";
-import sanitizeHtml from "sanitize-html";
+import { Link } from "react-router-dom";
+import React, { Fragment } from "react";
 
 const CommentContainer = styled.div`
   margin-top: 20px;
 `;
 const CommentCaption = styled.span`
   margin-left: 10px;
-  mark {
+  a {
     background-color: inherit;
     color: ${(p) => p.theme.accent};
     cursor: pointer;
@@ -23,15 +24,24 @@ interface IComments {
 }
 
 const Comment = ({ author, payload }: IComments) => {
-  const cleanedPayload = sanitizeHtml(
-    payload.replace(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g, "<mark>$&</mark>"),
-    { allowedTags: ["mark"] }
-  );
-
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption dangerouslySetInnerHTML={{ __html: cleanedPayload }} />
+      <CommentCaption>
+        {payload?.split(" ").map((word: string, index: number) =>
+          /#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g.test(word) ? (
+            <Link key={index} to={`/hashtags/${word.replace("#", "")}`}>
+              {word}{" "}
+            </Link>
+          ) : /@[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g.test(word) ? (
+            <Link key={index} to={`/users/${word.replace("@", "")}`}>
+              {word}{" "}
+            </Link>
+          ) : (
+            <React.Fragment key={index}>{word} </React.Fragment>
+          )
+        )}
+      </CommentCaption>
     </CommentContainer>
   );
 };
